88f01dc5248a4860266aefb34bad27fab2007657
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2021-05-04 09:13:10 +0200

    ReadMe: TOC as MarkDown

--- ReadMe_TOC.sh
+++ ReadMe_TOC.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
 f=ReadMe.md
+ft=`basename $0 .sh`.tmp
 
-grep '# ' $f | sed 's/# /#@/;s/  / /g;s/  / /g;s/  / /g;s/ /-/g;s/\.//g' | tr [:upper:] [:lower:] | sed 's/#@/# /;s/###/          - /;s/##/     - /;s/#/- /;'
+#get TOC
+grep '# ' $f > $ft
+
+#header
+echo 'Table of contents'
+echo
+
+#make MarkDown links
+while read l
+do
+#echo ":$l:"
+  #rank
+  
+  #title
+  t=`echo "$l" | sed 's/# //;s/#//g'`
+  #rank and link
+  k=`echo "$l" | sed 's/# /#@/;s/  / /g;s/  / /g;s/  / /g;s/ /-/g;s/\.//g' | tr [:upper:] [:lower:]`
+#echo "$t $k"
+  #gather
+  /bin/echo -e -n $k | sed "s/@/@[$t](/" | sed 's/#@/# /;s/###/          - /;s/##/     - /;s/#/- /;'
+  echo ')'
+done < $ft | sed 's/(/(\#/'
+
+#footer
+
+#clean
+rm $ft
